2578b2a2ea09656c3d9f2f7ec18f668d59792c5e
ksanthoshNT <kammari.santhosh@newgensoft.com>
2024-04-23 15:24:34 +0530

    python --name --age for fire

--- experiment_scripts/python_fire.py
+++ experiment_scripts/python_fire.py
@@ -1,7 +1,11 @@
+"""
+python -m experiment_scripts.python_fire --name hai --age adf
+
+"""
 import fire
 
-def hello(name):
-	return "Hello " + name
+def hello(name,age):
+	return "Hello " + name + age
 
 if __name__ == '__main__':
 	fire.Fire(hello)
